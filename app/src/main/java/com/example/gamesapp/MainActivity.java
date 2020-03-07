e4f7be5952a0ce78f2b066eb9643a8e5ea5a107d
nocerino <u.nocerino@studenti.uniba.it>
2020-03-07 21:44:47 +0100

    Launcher and Game List

--- app/src/main/java/com/example/gamesapp/MainActivity.java
+++ app/src/main/java/com/example/gamesapp/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.gamesapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.tb);
+        setSupportActionBar(toolbar);
+
+        final Button games = findViewById(R.id.games);
+
+        games.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                gamesList(view);
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
+    public void gamesList(View view) {
+        final Intent gamesList = new Intent(this, GameListActivity.class);
+        startActivity(gamesList);
+    }
+
 }
